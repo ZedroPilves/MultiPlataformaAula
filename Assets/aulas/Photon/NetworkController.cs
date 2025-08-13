fe91361091b57d27196e6403fdf9ff45fe95ed03
Pedro <pedro.zilves@gmail.com>
2025-08-13 11:25:38 -0300

    fim da aula

--- Assets/aulas/Photon/NetworkController.cs
+++ Assets/aulas/Photon/NetworkController.cs
@@ -4,19 +4,29 @@ using System.Collections.Generic;
 using Photon.Pun;
 using Photon.Realtime;
 using TMPro;
-using System;
+
+
 public class NetworkController : MonoBehaviourPunCallbacks
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     [SerializeField] bool connected = false;
 
-
+    [SerializeField] TMP_InputField playerNameInput;
     [SerializeField] TMP_Text btnTexto;
     [SerializeField] bool firstConnect = true;
+    [SerializeField] GameObject telaLogin;
+    [SerializeField] GameObject telaJoin;
+    [SerializeField] TMP_Text textoNickPlayer;
+    string playerNameTemp;
+
     void Start()
     {
-        
+        playerNameTemp = $"Player {Random.Range(1,10000)}";
+        playerNameInput.text = playerNameTemp;
+
+        telaLogin.SetActive(true);
+        telaJoin.SetActive(false);
     }
 
     // Update is called once per frame
@@ -34,6 +44,7 @@ public class NetworkController : MonoBehaviourPunCallbacks
         print("conectado");
         connected = true;   
         firstConnect = false;
+     
     }
 
     public void ConnectToServerBTN()
@@ -61,6 +72,9 @@ public class NetworkController : MonoBehaviourPunCallbacks
         }
     }
 
+
+    
+
     public void backToConnecting()
     {
 
@@ -71,7 +85,31 @@ public class NetworkController : MonoBehaviourPunCallbacks
         print("Conectado ao master");
         print($"Server: {PhotonNetwork.CloudRegion} \n Ping:{PhotonNetwork.GetPing()}");
         btnTexto.text = "Conectado";
+        Login();
+        PhotonNetwork.JoinLobby();
     }
+
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        base.OnJoinRandomFailed(returnCode, message);
+        string roomTemp = $"Room{Random.Range(1, 1000)}";
+        PhotonNetwork.CreateRoom(roomTemp);
+    }
+
+
+
+
+
+
+
+
     public override void OnDisconnected(DisconnectCause cause)
     {
         base.OnDisconnected(cause);
@@ -82,5 +120,29 @@ public class NetworkController : MonoBehaviourPunCallbacks
 
       connected = false;
         btnTexto.text = "reconectar";
+        telaLogin.SetActive(true);
+        telaJoin.SetActive(false);
     }
+
+    public void Login()
+    {
+        if (playerNameInput.text == "")
+        {
+            PhotonNetwork.NickName = playerNameTemp;
+            textoNickPlayer.text = playerNameTemp;
+
+        }
+        else
+        {
+            
+            PhotonNetwork.NickName = playerNameInput.text;
+            textoNickPlayer.text = playerNameInput.text;
+        }
+        telaLogin.SetActive(false);
+        telaJoin.SetActive(true);
+    }
+
+
+
+ 
 }
